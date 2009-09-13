9e13ed23b553ffcd2a5d859db6c5d12a5ebe4d1a
phj.arg <phj.arg@ed15059e-90ca-11de-8ad7-8b2c2f1e2016>
2009-09-13 00:16:09 +0000

    Adding targets "install" and "uninstall"

---
+++ trunk/include/odf_gen.h
@@ -0,0 +1,37 @@
+/*
+odf_gen: Simple API to generate OpenDocumen documents.
+    Copyright (C) 2009  Pablo Jorge, FuDePAN
+
+    This file is part of the odf-gen project.
+
+    odf-gen is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    odf-gen is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with odf-gen.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ODF_GEN_H
+#define ODF_GEN_H
+
+#ifdef ODF_GEN_NAMESPACE
+namespace odf_gen
+{
+#endif
+
+#ifndef NO_ODS_GENERATOR
+#   include <ods_generator.h>
+#endif
+
+#ifdef ODF_GEN_NAMESPACE
+}
+#endif
+
+#endif // ODS_GENERATOR_H
