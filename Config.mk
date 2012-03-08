0ac7bfffd7df5f76ef9eeb96e1860f22ba7dcf9d
phj.arg <phj.arg@gmail.com>
2012-03-08 05:22:31 +0000

    More fixes for the Python bindings to support python2.x

--- Config.mk
+++ Config.mk
@@ -7,6 +7,9 @@ PY_GET_LIB_PREFIX += print get_python_lib()
 PY_LIB_PREFIX=$(shell python -c "$(PY_GET_LIB_PREFIX)")
 PY_LIB_DIR=odfgen
 
+PY_INCLUDE=$(shell python-config --includes)
+PY_LIBS=$(shell python-config --libs)
+
 LIB_PREFIX=/usr/local/lib
 LIB_DIR=odfgen
 
