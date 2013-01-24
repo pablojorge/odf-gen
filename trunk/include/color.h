19243b8176453f841c39ccdc5fe6f075acbfd1c9
phj.arg@gmail.com <phj.arg@gmail.com@ed15059e-90ca-11de-8ad7-8b2c2f1e2016>
2013-01-24 02:26:26 +0000

    Change in licensing (Boost) and minor improvements

--- trunk/include/color.h
+++ trunk/include/color.h
@@ -1,37 +1,12 @@
-/*
-odf-gen: Simple API to generate OpenDocument documents.
-    Copyright (C) 2012  Pablo Jorge, FuDePAN
+// odf-gen: Simple API to generate OpenDocument documents.
+//
+//          Copyright Pablo Jorge 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
 
-    This file is part of the odf-gen project.
-
-    odf-gen is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    odf-gen is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with odf-gen.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*  As a special exception, you may create a larger work that contains
-    part of all of the odf-gen project and distribute that work under
-    the terms of the GNU General Public License as published by the
-    Free Software Foundation; version 2 of the License
-
-    Alternatively, if you modify or redistribute the ODF generator
-    itself, you may (ar your option) remove this special exception
-
-    This special exception was added by Pablo Jorge in 
-    version 1.3 of odf-gen. 
-*/
-
-#ifndef COLOR_H
-#define COLOR_H
+#pragma once
 
 #include <iostream>
 #include <iomanip>
@@ -102,4 +77,3 @@ private:
     int _idx;
 };
 
-#endif // COLOR_H
